e7d3aedd1775e37ade168b94c8fd2b661833d92e
ematkinsongit <emily.s.marin@icloud.com>
2021-12-02 13:41:33 -1000

    adding header component and links to the home and shop page
     #

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
 
+import "./App.css";
+import HomePage from "./pages/homepage/Homepage.js";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage.js";
+import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
